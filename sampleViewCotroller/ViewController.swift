7db391f7ece5f86943b039c2e1c06e27f25381ef
Sureshkumar <sureshkumar.m@greatinnovus.com>
2017-08-22 19:09:54 +0530

    First commit

---
+++ sampleViewCotroller/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  sampleViewCotroller
+//
+//  Created by Mac09 on 5/8/17.
+//  Copyright © 2017 Great Innovus Solutions. All rights reserved.
+//
+
+import UIKit
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
